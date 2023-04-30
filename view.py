190dc7d777ca9dafb0df3086f717fd2d9c331e78
Rene Arrieta <renearrieta8905@gmail.com>
2023-04-30 17:08:32 -0500

    Añadida clase VistaSolido

--- view.py
+++ view.py
@@ -10,3 +10,15 @@ class VistaFigura:
     def mostrar_resultado(self, area, perimetro):
         print("El área es:", area, " unidades cuadradas")
         print("El perímetro es:", perimetro, " unidades")
+
+
+class VistaSolido:
+    def pedir_datos(self):
+        opcion = input(
+            "Seleccione el sólido que desea calcular (parámetros requeridos se indican en paréntesis):\n1. Cubo (arista)\n2. Esfera (radio)\n3. Cilindro (radio y altura)\n4. Pirámide (lado de la base y altura)\n5. Cono (radio y altura)\n")
+        medidas = input("Ingrese las medidas separadas por comas sin espacios: ").split(",")
+        medidas = [float(medida) for medida in medidas]
+        return opcion, medidas
+
+    def mostrar_resultado(self, volumen):
+        print("El volumen es:", volumen, " unidades cúbicas")
