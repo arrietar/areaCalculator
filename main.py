a38ee49d253f0771101b970950a178a47f12e512
Rene Arrieta <renearrieta8905@gmail.com>
2023-04-30 17:41:03 -0500

    Modificado archivo main.py

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from model import Figura, Solido
+from view import VistaFigura, VistaSolido
 
 
-# Press the green button in the gutter to run the script.
+def main():
+    opcion_figura = input("¿Desea calcular áre y perímetro de una figura? (s/n): ")
+    if opcion_figura.lower() == "s":
+        vista_figura = VistaFigura()
+        opcion, medidas = vista_figura.pedir_datos()
+        figura = Figura(opcion, medidas)
+        area, perimetro = figura.calcular_area_perimetro()
+        vista_figura.mostrar_resultado(area, perimetro)
+
+    opcion_solido = input("¿Desea calcular volumen de un sólido? (s/n): ")
+    if opcion_solido.lower() == "s":
+        vista_solido = VistaSolido()
+        opcion, medidas = vista_solido.pedir_datos()
+        solido = Solido(opcion, medidas)
+        volumen = solido.calcular_volumen()
+        vista_solido.mostrar_resultado(volumen)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
+    main()
