fbf97fca4261611eb27e543eed6a2befef2f39d4
Rene Arrieta <renearrieta8905@gmail.com>
2023-04-30 17:24:37 -0500

    Agregada clase ControladorSolido

--- control.py
+++ control.py
@@ -14,3 +14,16 @@ class ControladorFigura:
         self.figura.medidas = medidas
         area, perimetro = self.figura.calcular_area_perimetro()
         self.vista.mostrar_resultado(area, perimetro)
+
+
+class ControladorSolido:
+    def __init__(self):
+        self.solido = Solido()
+        self.vista = VistaSolido()
+
+    def calcular_volumen(self):
+        opcion, medidas = self.vista.pedir_datos()
+        self.solido.opcion = opcion
+        self.solido.medidas = medidas
+        volumen = self.solido.calcular_volumen()
+        self.vista.mostrar_resultado(volumen)
